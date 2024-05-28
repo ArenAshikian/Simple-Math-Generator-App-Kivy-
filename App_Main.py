7d3bf640a68693690ef8fb0d82bd84d3f6225c61
ArenAshikian <46458166+ArenAshikian@users.noreply.github.com>
2024-05-28 02:27:45 -0700

    Fixed code

--- App_Main.py
+++ App_Main.py
@@ -1,127 +1,97 @@
 import kivy
 import random
-import time
-import sys
 import pygame
 from pygame import mixer
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.progressbar import ProgressBar
+from kivy.uix.boxlayout import BoxLayout
 
-score=0
-timer_switch=True
-counter=0
+score = 0
 
+# Load Kivy file
 Builder.load_file('App_Main.kv')
 
-class MyGrid(GridLayout):
+class MyGrid(BoxLayout):
     pygame.init()
-    #mixer.music.load('background.wav')
-    #mixer.music.play(-1)
+    mixer.init()
+    # Uncomment the lines below if you have a background music file
+    # mixer.music.load('background.wav')
+    # mixer.music.play(-1)
 
     def addition(self):
-        answer=0
-        question=""
-        number_One=0
-        number_Two=0
-        
-        number_One = random.randint(0,20)
-        number_Two = random.randint(0,20)
-        self.question="What is " + str(number_One) + " + " +str(number_Two)
-        self.answer= (number_One + number_Two)
+        number_One = random.randint(0, 20)
+        number_Two = random.randint(0, 20)
+        question = f"What is {number_One} + {number_Two}?"
+        answer = number_One + number_Two
         return question, answer
 
-    def __init__ (self, **kwargs):
+    def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
-        self.cols=1
-        self.inside = GridLayout()
-        self.current_health=200
-        self.maximum_health=1000
-        self.health_bar_length=400
-        self.health_ratio=self.maximum_health/self.health_bar_length
-   
-        #self.basic_health()
-        self.addition()
-        self.inside.cols=1
-        self.inside.add_widget(Label(text=self.question)) 
-        self.name=TextInput(multiline=False)
-        self.inside.add_widget(self.name)
-        self.add_widget(self.inside)
-        self.submit = Button(text="Submit", font_size=40)
-        self.submit.bind(on_press=self.pressed)
-        self.add_widget(self.submit)
+        self.orientation = 'vertical'
         
+        self.current_health = 100
+        self.maximum_health = 100
 
+        self.progress_bar = self.ids.my_progress_bar
 
+        self.inside = self.ids.inside_grid
+        self.submit_button = self.ids.submit_button
+        self.submit_button.bind(on_press=self.pressed)
+
+        self.load_question()
+
+    def load_question(self):
+        self.inside.clear_widgets()
+        self.question, self.answer = self.addition()
+        self.inside.add_widget(Label(text=self.question))
         
-
+        self.name_input = TextInput(multiline=False)
+        self.inside.add_widget(self.name_input)
         
-
-
     def pressed_answer(self, instance):
-        Builder.unload_file('App_Main.kv')
-        self.remove_widget(self.answer)
-        self.__init__()
-        
+        self.load_question()
 
     def pressed_restart(self, instance):
         global score
-        self.remove_widget(self.restart)
-        self.remove_widget(self.ending)
-        current= self.ids.my_progress_bar.value
-        current=100
-        
-        self.ids.my_progress_bar.value=current
-        score=0
-        self.__init__()
-        
-    
+        score = 0
+        self.current_health = 100
+        self.progress_bar.value = self.current_health
+        self.load_question()
 
     def pressed(self, instance):
         global score
-        global counter
-
         
-        if str(self.answer)==self.name.text:
-            
-            score+=1
-            self.remove_widget(self.inside)
-            self.remove_widget(self.submit)
-            current= self.ids.my_progress_bar.value
-            current+=10
-            if current>100:
-                current=100
-            self.ids.my_progress_bar.value=current
-            self.answer = Button(text="Great Job!", font_size=40)
-            self.answer.bind(on_press=self.pressed_answer)
-            self.add_widget(self.answer)
+        if str(self.answer) == self.name_input.text:
+            score += 1
+            self.current_health += 10
+            if self.current_health > self.maximum_health:
+                self.current_health = self.maximum_health
 
-
-            
-        else: 
-            current= self.ids.my_progress_bar.value
-            current-=10
-            self.ids.my_progress_bar.value=current
-            if current<=0:
-                
-                Builder.unload_file('App_Main.kv')
-                Builder.unload_file('App_Main.kv')
-                self.ids.my_progress_bar.value=1
-                self.remove_widget(self.inside)
-                self.remove_widget(self.submit)
-                self.ending=GridLayout()
-                self.ending.cols=1
-                self.ending.add_widget(Label(text="You Lose", font_size=40))
-                self.restart = Button(text="          Press to Try Again!\n You had " + str(score)+ " Questions Correct!", font_size=40)
-                self.restart.bind(on_press=self.pressed_restart)
-                self.add_widget(self.restart)
-
-        self.name.text=""      
+            self.inside.clear_widgets()
+            self.inside.add_widget(Label(text="Great Job!", font_size=40))
+            next_question_button = Button(text="Next Question", font_size=40)
+            next_question_button.bind(on_press=self.pressed_answer)
+            self.inside.add_widget(next_question_button)
+        else:
+            self.current_health -= 10
+            if self.current_health <= 0:
+                self.current_health = 0
+                self.inside.clear_widgets()
+                self.inside.add_widget(Label(text="You Lose", font_size=40))
+                restart_button = Button(text=f"Press to Try Again! You had {score} Questions Correct!", font_size=40)
+                restart_button.bind(on_press=self.pressed_restart)
+                self.inside.add_widget(restart_button)
+            else:
+                self.load_question()
         
+        self.progress_bar.value = self.current_health
+        self.name_input.text = ""
+
 class MyApp(App):
     def build(self):
         return MyGrid()
